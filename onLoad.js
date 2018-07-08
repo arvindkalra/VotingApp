6f2b5f0c8e703965e197a62031c2bc542a462f1b
arvindkalra <kalarvind97@gmail.com>
2018-07-08 16:38:52 +0530

    new contract deployed

--- onLoad.js
+++ onLoad.js
@@ -1,9 +1,15 @@
-// let web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/ZWXhYfP2uIvdg1yKuQNY"));
+// let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 let self = web3.eth.accounts[0];
-let abi = [{"constant":false,"inputs":[{"name":"_uid","type":"uint256"}],"name":"voteAgainst","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumberOfQuestions","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_uid","type":"uint256"}],"name":"getQuestionDetails","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_question","type":"string"}],"name":"addQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_uid","type":"uint256"}],"name":"voteInFavour","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"infavour","type":"uint256"},{"indexed":false,"name":"against","type":"uint256"},{"indexed":false,"name":"question","type":"string"}],"name":"newQuestionAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"infavour","type":"uint256"},{"indexed":false,"name":"against","type":"uint256"}],"name":"updatedQuestion","type":"event"}];
+let abi = [{"constant":false,"inputs":[{"name":"_uid","type":"uint256"}],"name":"voteAgainst","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumberOfQuestions","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_uid","type":"uint256"}],"name":"deleteQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_uid","type":"uint256"}],"name":"getQuestionDetails","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_question","type":"string"}],"name":"addQuestion","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_uid","type":"uint256"}],"name":"voteInFavour","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"infavour","type":"uint256"},{"indexed":false,"name":"against","type":"uint256"},{"indexed":false,"name":"question","type":"string"}],"name":"newQuestionAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"infavour","type":"uint256"},{"indexed":false,"name":"against","type":"uint256"}],"name":"updatedQuestion","type":"event"}];
 let VotingContract = web3.eth.contract(abi);
-let contractInstance = VotingContract.at('0xc6222f19e79216aa65f87701d272461878d60737');
+let contractInstance = VotingContract.at('0x433234739bcff02a401a1dbabb5c88b4e87ca6dc');
 let total = 0;
+
+function getNum(){
+    contractInstance.getNumberOfQuestions({from: self}, function (err, result) {
+        console.log(result.valueOf());
+    });
+}
 
 $(document).ready(function () {
     contractInstance.getNumberOfQuestions({from: self}, function (err, result) {
@@ -11,16 +17,30 @@ $(document).ready(function () {
         let number = result.valueOf();
         total = number;
         for(let i = 0; i < number; i++){
-            getQuestionDetails(i, function (question, infavour, against) {
-                $('.questions').append('<section class="votebox" id="' + i + '">\n' +
-                    '        <p class="pollid">Poll ID: ' + i + ' </p>\n' +
-                    '        <p class="question">' + question +
-                    '        </p>\n' +
-                    '        <button class="pollbutton yes" onclick="onClickYes('+i+')">Yes</button>\n' +
-                    '        <button class="pollbutton no" onclick="onClickNo('+i+')">No</button>\n' +
-                    '        <p class="infavour">Votes For Yes : ' + infavour + '</p>\n' +
-                    '        <p class="against">Votes For No : ' + against + '</p>' +
-                    '    </section>')
+            getQuestionDetails(i, function (question, infavour, against, isActive, canVote) {
+                if(isActive == 1){
+                    if(canVote){
+                        $('.questions').append('<section class="votebox" id="' + i + '">\n' +
+                            '        <p class="pollid">Poll ID: ' + i + ' </p>\n' +
+                            '        <p class="question">' + question +
+                            '        </p>\n' +
+                            '        <button class="pollbutton yes" onclick="onClickYes('+i+')">Yes</button>\n' +
+                            '        <button class="pollbutton no" onclick="onClickNo('+i+')">No</button>\n' +
+                            '        <button class="pollbutton voted" style="display: none">Your Transaction is Being Mined</button>' +
+                            '        <p class="infavour">Votes For Yes : ' + infavour + '</p>\n' +
+                            '        <p class="against">Votes For No : ' + against + '</p>' +
+                            '    </section>');
+                    }else{
+                        $('.questions').append('<section class="votebox" id="' + i + '">\n' +
+                            '        <p class="pollid">Poll ID: ' + i + ' </p>\n' +
+                            '        <p class="question">' + question +
+                            '        </p>\n' +
+                            '        <button class="pollbutton voted">You Have Already Voted For this Question</button>\n' +
+                            '        <p class="infavour">Votes For Yes : ' + infavour + '</p>\n' +
+                            '        <p class="against">Votes For No : ' + against + '</p>' +
+                            '    </section>');
+                    }
+                }
             });
         }
     });
@@ -29,7 +49,7 @@ $(document).ready(function () {
         contractInstance.getQuestionDetails(uid, {from: self}, function (err, result) {
             if(err) throw err;
             let arr = result.valueOf();
-            callback(arr[0], arr[1].valueOf(), arr[2].valueOf());
+            callback(arr[0], arr[1].valueOf(), arr[2].valueOf(), arr[3].valueOf(), arr[4].valueOf());
         });
     }
 
@@ -62,6 +82,7 @@ $(document).ready(function () {
                     '        </p>\n' +
                     '        <button class="pollbutton yes" onclick="onClickYes('+uid+')">Yes</button>\n' +
                     '        <button class="pollbutton no" onclick="onClickNo('+uid+')">No</button>\n' +
+                    '        <button class="pollbutton voted" style="display: none">You Have Already Voted For this Question</button>' +
                     '        <p class="infavour">Votes For Yes : ' + infavour + '</p>\n' +
                     '        <p class="against">Votes For No : ' + against + '</p>' +
                     '    </section>')
@@ -70,17 +91,16 @@ $(document).ready(function () {
     }
 
     function makeUpdate(uid, infavour, against) {
-        console.log("Called", uid);
         let str = "#" + uid;
         let $infavour = $(str).children('.infavour').html('Votes For Yes : ' + infavour + '');
-        let $against = $(str).children('.against').html('Votes For Yes : ' + against + '');
+        let $against = $(str).children('.against').html('Votes For No : ' + against + '');
     }
 
 
 });
 
 function voteInFavour(uid) {
-    contractInstance.voteInFavour(uid, {from: self , gas: 35000, gasPrice: web3.toWei(40,'gwei')}, function (err) {
+    contractInstance.voteInFavour(uid, {from: self, gas: 70000, gasPrice: web3.toWei(40,'gwei')}, function (err) {
         if(err){
             alert("You Have Already Voted For this Question");
         }
@@ -88,7 +108,7 @@ function voteInFavour(uid) {
 }
 
 function voteAgainst(uid) {
-    contractInstance.voteAgainst(uid, {from: self , gas: 35000, gasPrice: web3.toWei(40,'gwei')}, function (err) {
+    contractInstance.voteAgainst(uid, {from: self, gas: 70000, gasPrice: web3.toWei(40,'gwei')}, function (err) {
         if(err){
             if(err){
                 alert("You Have Already Voted For this Question");
@@ -98,14 +118,27 @@ function voteAgainst(uid) {
 }
 
 function onClickYes(i) {
+    let str = "#" + i;
+    $(str).children('.yes').css('display', 'none');
+    $(str).children('.no').css('display', 'none');
+    $(str).children('.voted').css('display', 'inline-block');
     voteInFavour(i);
 }
 
 function onClickNo(i) {
+    let str = "#" + i;
+    $(str).children('.yes').css('display', 'none');
+    $(str).children('.no').css('display', 'none');
+    $(str).children('.voted').css('display', 'inline-block');
     voteAgainst(i);
 }
 
 function addQuestion(val) {
-    contractInstance.addQuestion(val, {from: self , gas: 70000, gasPrice: web3.toWei(40,'gwei')}, function (err, result) {
+    contractInstance.addQuestion(val, {from: self , gas: 200000, gasPrice: web3.toWei(40,'gwei')}, function (err, result) {
+    });
+}
+
+function deleteQuestion(val) {
+    contractInstance.deleteQuestion(val, {from: self, gas: 70000, gasPrice: web3.toWei(40,'gwei')}, function (err, result) {
     });
 }
